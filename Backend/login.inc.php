c877e105e3e57718b6c45cb6a5c4ba4bb7b62fc5
Ravindu Deshan K Withana <43211060+RavinduDeshank@users.noreply.github.com>
2020-05-07 19:56:32 +0530

    Update login.inc.php

--- Backend/login.inc.php
+++ Backend/login.inc.php
@@ -1,4 +1,5 @@
 <?php
+
 include 'dbconnection.php';
 if(isset($_POST['login'])){
     $errors = array();
@@ -6,27 +7,27 @@ if(isset($_POST['login'])){
     $uname = mysqli_real_escape_string($db,$_POST['name']);
     $pass = mysqli_real_escape_string($db, $_POST['pwd']);
 
-    $sql = "SELECT * FROM taxicustomer WHERE username='$uname' AND password='$pass'";
+    $sql = "SELECT * FROM users WHERE uname='$uname' AND upass='$pass'";
     $result = mysqli_query($db,$sql);
 
     if($result){
         if (mysqli_num_rows($result) > 0) {
 
             $row = mysqli_fetch_assoc($result);
-            $usertype = $row['user_type'];
+            $usertype = $row['utype'];
             if ($usertype == "Admin") {
                 session_start();
-                $_SESSION['user_id'] = $row['user_id'];
-                $_SESSION['username'] = $row['username'];
+                $_SESSION['userid'] = $row['uId'];
+                $_SESSION['username'] = $row['uname'];
                 header("Location: admin.php");
                 exit();
             }
 
             if ($usertype == "Customer") {
                 session_start();
-                $_SESSION['user_id'] = $row['user_id'];
-                $_SESSION['username'] = $row['username'];
-                header("Location: viewtaxi.php");
+                $_SESSION['userid'] = $row['uId'];
+                $_SESSION['username'] = $row['uname'];
+                header("Location: userPanel.php");
                 exit();
             } 
             
