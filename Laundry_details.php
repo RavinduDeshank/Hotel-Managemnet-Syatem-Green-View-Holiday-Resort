4eea86d74ac33187d6431c8503adf8a29f4896ee
rashmika1998 <rvishwwamal@gmail.com>
2020-05-13 19:27:04 -0700

    add

--- Laundry_details.php
+++ Laundry_details.php
@@ -238,8 +238,8 @@ $result = mysqli_query($db, $sql);
         </div>
         <!-- Footer End-->
     </footer>
-    
-    <s-->
+
+    <!-- JS here -->
 
     <!-- All JS Custom Plugins Link Here here -->
     <script src="./assets/js/vendor/modernizr-3.5.0.min.js"></script>
