1d0062ff33faf2d718b62afadca98c1ab1d51ef3
shivanthi-fernando <58467116+shivanthi-fernando@users.noreply.github.com>
2020-05-17 17:25:18 +0530

    hall database edit

--- hall_db_connection.php
+++ hall_db_connection.php
@@ -1,6 +1,6 @@
 <?php
   //Hall database connection
-  $con=mysqli_connect("localhost","","","greenview");
+  $con=mysqli_connect("localhost","root","","greenview");
   if (mysqli_connect_errno())
   {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
